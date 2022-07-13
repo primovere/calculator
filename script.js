234829d682cfb80600629f91ea1d3727f32ce9af
primovere <p.8izo3@slmail.me>
2022-07-13 16:10:10 +0800

    Add operate() and HTML boilerplate

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operate(operator, x, y) {
+  if (operator === "+") return add(x, y);
+  if (operator === "-") return subtract(x, y);
+  if (operator === "*") return multiply(x, y);
+  if (operator === "/") return divide(x, u);
+}
